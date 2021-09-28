af3c2eedaa809816976886a3c993e74a016a5634
Muzammal Murtaza <68580311+muzammal-murtaza@users.noreply.github.com>
2021-09-28 01:37:53 -0700

    Header module (#1)
    * Add header module
    
    * Complete styling of header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,10 +1,22 @@
-import {Fragment} from 'react';
-const Header = (props)=> {
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import classes from './Header.module.css';
+import mealsImage from '../../assets/meals.jpeg';
+const Header = (props) => {
   return (
     <Fragment>
-      <h1>React meals</h1>
+      <header className={classes.header}>
+        <h1>React meals</h1>
+        <HeaderCartButton />
+      </header>
+      <img
+        className={classes['main-image']}
+        src={mealsImage}
+        alt="header pic"
+      />
     </Fragment>
   );
-}
+};
 
 export default Header;
